fc181a6d4959fc550fcdaab7d8ccec12f8baa420
batheand <batheand@gmail.com>
2023-06-03 16:08:27 +0300

    comments

--- Select_Querrys.sql
+++ Select_Querrys.sql
@@ -1,3 +1,16 @@
+--Filtrelemelerde bir string commandi alıp onun bulunduğu sonuçları getirecek kodlar lazım.
+
+-- [ ] Filter Flights by Airport Location
+-- [ ] Filter Flights by Airport Name
+-- [ ] Filter Flights by Plane ID
+-- [ ] Filter Flights by Plane Brand
+-- [ ] Filter Flights by Plane Airline
+-- [ ] Filter Flights by Plane Model
+-- [ ] Sort by Name (employee)
+-- [ ] Sort by Job Title (employee)
+-- [ ] Sort by Number of flights (employee)
+-- [ ] Sort by Total Flight Hours (employee)
+
 SELECT *
 FROM FlightCompany.Employee A
 ORDER BY A.Employee_ID;
